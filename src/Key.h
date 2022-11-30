6919c47c66f3a548b3030d9ef2f13d0a477e58a7
Leon Wang <leonw2@illinois.edu>
2022-11-30 21:00:48 +0000

    initial commit

---
+++ src/Key.h
@@ -0,0 +1,104 @@
+/**
+ * @file Key.h
+ *
+ * Implementation of a data structure that defines a piano key as a subset of the
+ * primitive character datatype with a custom conversion method to integer.
+ *
+ * @author Leon Wang
+ * @date Fall 2022
+ */
+
+#pragma once
+
+#include <map>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <stdexcept>
+#include "cs225/HSLAPixel.h"
+
+struct Key 
+{
+   char key;
+   int value;
+
+   /**
+    * Default constructor
+    */
+   Key() : key('C'), value(getValue('C')) {}
+
+   /**
+    * Constructor
+    * @param c the character
+    */
+   Key(char c) : key(isKey(c)), value(getValue(c)) {}
+
+   /**
+    * Constructor
+    * @param pixel the HSLAPixel
+    */
+   Key(const cs225::HSLAPixel &pixel) : key(getKey(pixel)), 
+                                        value(getValue(getKey(pixel))) {}
+
+   /**
+    * Checks if input character is a valid key
+    * @param c the character
+    * @return the character if it was a valid key 
+    */ 
+   char isKey(char c) {
+     std::map<char,int> toInt = {
+                                {'C',0},{'c',1},{'D',2},{'d',3},{'E',4},{'F',5},
+                                {'f',6},{'G',7},{'g',8},{'A',9},{'a',10},{'B',11}
+                                }; 
+     if (toInt.find(c) == toInt.end()) {
+        throw std::invalid_argument("Not a valid Key");
+     }
+     return c;
+   }
+
+   /**
+    * Character-to-integer conversion method
+    * @param c the character
+    * @return the integer form of the char based on conversion tables
+    */
+   int getValue(char c) {
+      std::map<char,int> toInt = {
+                                 {'C',0},{'c',1},{'D',2},{'d',3},{'E',4},{'F',5},
+                                 {'f',6},{'G',7},{'g',8},{'A',9},{'a',10},{'B',11}
+                                 };
+      return toInt[c];
+   }
+
+   /**
+    * Pixel-to-key conversion method
+    * @param pixel the HSLAPixel
+    * @return the char (Key) representation of the pixel
+    */ 
+   char getKey(const cs225::HSLAPixel &pixel) {
+      std::map<int,char> toChar = {
+                                  {0,'C'},{1,'c'},{2,'D'},{3,'d'},{4,'E'},{5,'F'},
+                                  {6,'f'},{7,'G'},{8,'g'},{9,'A'},{10,'a'},{11,'B'}
+                                  };
+      return isKey(toChar[(int) std::fmod(pixel.h, 12)]);
+   }
+};
+
+   /**
+    * Overload operator <<
+    * @param out Output stream
+    * @param thisKey The Key to print
+    * @return Output stream
+    */
+inline std::ostream & operator<<(std::ostream &out, Key const &thisKey) {
+   std::map<char,std::string> sharps = {
+                                       {'c',"C#"},{'d',"D#"},{'f',"F#"},{'g',"G#"},{'a',"A#"}
+                                       };
+   if (sharps.find(thisKey.key) != sharps.end()) {
+      out << sharps[thisKey.key];
+   }
+   else {
+      out << thisKey.key;
+   }
+   return out;
+}
---
+++ src/Note.h
@@ -0,0 +1,83 @@
+/**
+ * @file Note.h
+ *
+ * Implementation of a data structure that represents piano notes as a superset of the
+ * Key data type, adding a range of ten octave levels and distance measurement. 
+ *
+ * @author Leon Wang
+ * @date Fall 2022
+ */
+
+#pragma once
+
+#include "Key.h"
+
+struct Note : public Key
+{
+
+   Key key;
+   unsigned int octave;
+
+   /**
+    * Default constructor, set at 5C (C key on 5th octave)
+    */
+   Note() : key(Key()), octave(5-1) {}
+
+   /**
+    * Key constructor
+    * @param key on the piano roll
+    * @param octave level the note is in (limit 10 octaves)
+    */
+   Note(Key key, unsigned int octave) : key(key), octave((octave-1)%10) {}
+
+   /**
+    * Character constructor
+    * @param c the character
+    * @param octave level the note is in (limit 10 octaves)
+    */
+   Note(char c, unsigned int octave) : key(Key(c)), octave((octave-1)%10) {}
+
+   /**
+    * HSLAPixel constructor
+    * @param pixel the HSLAPixel
+    */
+   Note(const cs225::HSLAPixel &pixel) : key(Key(pixel)), 
+                                         octave(getOctave(pixel)) {}
+
+   /**
+    * Distance method
+    * @returns the number of keys from C0 (0) to the Key
+    */
+   int distance() const {
+      return key.value + (octave*10);
+   }
+
+   /**
+    * Overload operator -
+    * @param rhs The other Note
+    * @return interval between two Notes (UNSCALED, scale to one octave using mod 10)
+    */
+   int operator-(const Note &rhs) {
+      return std::abs(this->distance() - rhs.distance()) - 1;
+   }
+
+   /**
+    * Pixel-to-octave conversion method
+    * @param pixel the HSLAPixel
+    * @return the octave representation of the pixel
+    */
+   int getOctave(const cs225::HSLAPixel &pixel) {
+      return (int) std::fmod(pixel.s*10, 10);
+   }
+};
+
+   /**
+    * Overload operator <<
+    * @param out Output stream
+    * @param note The Note to print
+    * @return Output stream
+    */
+inline std::ostream & operator<<(std::ostream &out, Note const &note) {
+   out << note.octave+1 << note.key;
+   return out;
+}
