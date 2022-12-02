af5dd2b24ff9c4f670de1e2559ed9f95eba398b2
Leon Wang <leonw2@illinois.edu>
2022-12-02 06:06:12 +0000

    basic NoteGraph test

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "NoteGraph.h"
 
 using namespace std;
 
 int main()
 {
+    NoteGraph test = NoteGraph("../images/basic/5x5.png");
+    test.printGraph();
     return 0;
 }
