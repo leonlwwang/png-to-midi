39ad47c75cc4947a69fed23c857abfef378faaa9
Leon Wang <leonw2@illinois.edu>
2022-12-07 04:19:51 +0000

    AStar completed 😮‍💨

--- src/algorithms/AStar.h
+++ src/algorithms/AStar.h
@@ -19,20 +19,12 @@ namespace notealgorithm {
        a minimum spanning tree out of the pathway.
      * Since a NoteGraph is a grid-like graph with a maximum of four directions 
        from each node, we opted to use Manhattan Distance as the heuristic.
+     * Internally, A* picks the next node based on F, a parameter based on
+       the sum of G and H. G is the cost of moving from the current node to the
+       candidate node, and H is the Manhattan Distance starting from the candidate
+       node to the very last node.
      * @param graph the NoteGraph
      * @return the Minimum Spanning Tree (MST)
      */
     NoteGraph aStar(const NoteGraph &graph);
-
-    /**
-     * Custom Manhattan distance calculator that
-       calculates the Manhattan distance on a weighted
-       graph from one vertex to another.
-     * @param start starting vertex
-     * @param end destination vertex
-     * @param graph the NoteGraph
-     * @return the sum of the edges that the path traverses 
-               through between the two vertices
-     */
-    int manh(int start, int end, const NoteGraph &graph);
 }
