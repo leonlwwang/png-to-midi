512e7e108b9b8a2e0b4478de026bf6897a4a5583
Leon Wang <leonw2@illinois.edu>
2022-12-11 22:06:25 +0000

    demo code on main

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include "NoteGraph.h"
+#include "algorithms/Prim.h"
+#include "algorithms/AStar.h"
 
 using namespace std;
 
 int main()
 {
+    /* full demonstration */
+    std::cout << "\n\t\t\t\t\t\tORIGINAL GRAPH \n\n";
     NoteGraph test = NoteGraph("../images/basic/5x5.png");
     test.printGraph();
+    std::cout << "\n\t\t\t\t\t\tPRIM MST \n\n";
+    NoteGraph primMST = notealgorithm::prim(test);
+    primMST.printGraph();
+    std::cout << "\n\t\t\t\t\t\tASTAR MST \n\n";
+    NoteGraph aStarMST = notealgorithm::aStar(test);
+    aStarMST.printGraph();
     return 0;
 }
