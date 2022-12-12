dc16f705b04667791d60d024202170881f5f7968
Leon Wang <leonw2@illinois.edu>
2022-12-12 06:02:51 +0000

    add to demo code on main

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,6 +1,8 @@
+#include <list>
 #include <iostream>
 #include "algorithms/Prim.h"
 #include "algorithms/AStar.h"
+#include "algorithms/DFS.h"
 
 using namespace std;
 
@@ -10,11 +12,35 @@ int main()
     std::cout << "\n\t\t\t\t\t\tORIGINAL GRAPH \n\n";
     NoteGraph test = NoteGraph("../images/basic/5x5.png");
     test.printGraph();
+
+    std::cout << "\nDFS:\t";
+    std::list<int> og = notealgorithm::DFS(test);
+    for (auto i : og) {
+        std::cout << i << " -> ";
+    }
+    std::cout << '\n';
+
     std::cout << "\n\t\t\t\t\t\tPRIM MST \n\n";
     NoteGraph primMST = notealgorithm::prim(test);
     primMST.printGraph();
+
+    std::cout << "\nDFS:\t";
+    std::list<int> prim = notealgorithm::DFS(primMST);
+    for (auto i : prim) {
+        std::cout << i << " -> ";
+    }
+    std::cout << '\n';
+
     std::cout << "\n\t\t\t\t\t\tASTAR MST \n\n";
     NoteGraph aStarMST = notealgorithm::aStar(test);
     aStarMST.printGraph();
+
+    std::cout << "\nDFS:\t";
+    std::list<int> aStar = notealgorithm::DFS(aStarMST);
+    for (auto i : aStar) {
+        std::cout << i << " -> ";
+    }
+    std::cout << "\n\n";
+
     return 0;
 }
