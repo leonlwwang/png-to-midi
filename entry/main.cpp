d87116e8073f935b7009203807a6b4e3fb0f12c5
Patrick Han <ph8@illinois.edu>
2022-12-12 01:49:49 +0000

    Added note tests

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include "NoteGraph.h"
 
-using namespace std;
+#include "algorithms/Prim.h"
+#include "algorithms/AStar.h"
 
 int main()
 {
-    NoteGraph test = NoteGraph("../images/basic/5x5.png");
-    test.printGraph();
+    NoteGraph n = NoteGraph("../images/basic/3x3.png");
+    std::cout << "Original Graph\n\n";
+    n.printGraph();
+
+    /*
+    NoteGraph primMST = notealgorithm::prim(n);
+    std::cout << "\nPrim's Algorithm\n\n";
+    primMST.printGraph();
+    NoteGraph aStarMST = notealgorithm::aStar(n);
+    std::cout << "\nA* Algorithm\n\n";
+    aStarMST.printGraph();
+    */
+    
     return 0;
 }
